9a95d7d3b3ef5561a50bebd02112c3f1b051a5e6
eg_kim <kimsagwa00@gmail.com>
2024-10-09 14:11:31 +0900

    title animation try

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -1,41 +1,40 @@
-let userCurrent = 0;
+$(document).ready(function() {
+    // fullpage.js 초기화
+    $('#fullpage').fullpage({
+        autoScrolling: true,  // fullpage.js 옵션 설정
+        //navigation: true,
+        afterRender: function() {
+            var textWrapper = $('.TitleAni');
+            if (textWrapper.length > 0) {
+                textWrapper.html(textWrapper.text().replace(/\S/g, "<span class='TitleAnimation'>$&</span>"));
 
-const totalSection = document.querySelectorAll('.section').length;
-
-function scrollToSection(sectionIndex){
-    const sections = document.querySelectorAll('.section');
-    const section = sections[sectionIndex];
-
-    console.log('sectionIndex:', sectionIndex);
-    console.log('sections:', sections.length);
-
-    window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth'
-    })
-
-}
-
-function throttle(func,limit){
-    let inThrottle;
-    return function(...args){
-        if(!inThrottle){
-            func.apply(this,args);
-            inThrottle=true;
-            setTimeout(()=>inThrottle=false,limit)
+                anime.timeline({loop: true})
+                    .add({
+                        targets: '.TitleAnimation',
+                        scale: [0.3, 1],
+                        opacity: [0, 1],
+                        translateZ: 0,
+                        easing: "easeOutExpo",
+                        duration: 600,
+                        delay: (el, i) => 70 * (i + 1)
+                    }).add({
+                        targets: '.TitleAnimation',
+                        scaleX: [0, 1],
+                        opacity: [0.5, 1],
+                        easing: "easeOutExpo",
+                        duration: 700,
+                        offset: '-=875',
+                        delay: (el, i, l) => 80 * (l - i)
+                    }).add({
+                        targets: '.TitleAnimation',
+                        opacity: 0,
+                        duration: 1000,
+                        easing: "easeOutExpo",
+                        delay: 1000
+                    });
+            } else {
+                console.error("Element with class 'TitleAni' not found.");
+            }
         }
-    }
-}
-
-function handleScroll(event){
-    event.preventDefault;
-    if(event.deltaY>0 && userCurrent < totalSection -1){
-        userCurrent++;
-        }
-    else if(event.deltaY< 0 && userCurrent > 0){
-        userCurrent--;
-        }
-    scrollToSection(userCurrent)
-}
-
-window.addEventListener('wheel', throttle(handleScroll,300))
+    });
+});
