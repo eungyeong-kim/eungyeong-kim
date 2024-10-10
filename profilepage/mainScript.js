9d88ee234a7021e030c72699151ee5b6ba0ac9a0
eg_kim <kimsagwa00@gmail.com>
2024-10-10 15:00:02 +0900

    firstpage animation

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -1,10 +1,5 @@
-$(document).ready(function() {
-    // fullpage.js 초기화
-    $('#fullpage').fullpage({
-        autoScrolling: true,  // fullpage.js 옵션 설정
-        //navigation: true,
-        afterRender: function() {
-            var textWrapper = $('.TitleAni');
+$(document).ready(function(){
+    var textWrapper = $('.TitleAni');
             if (textWrapper.length > 0) {
                 textWrapper.html(textWrapper.text().replace(/\S/g, "<span class='TitleAnimation'>$&</span>"));
 
@@ -15,18 +10,39 @@ $(document).ready(function() {
                         opacity: [0, 1],
                         translateZ: 0,
                         easing: "easeOutExpo",
+                        duration: 900,
+                        delay: (el, i) => 110 * (i + 1)
+                    }).add({
+                        targets: '.TitleAnimation',
+                        opacity: 0,
+                        duration: 900,
+                        easing: "easeOutExpo",
+                        delay: 1100
+                    });
+            } else {
+                console.error("Element with class 'TitleAni' not found.");
+            }
+            var textWrapper = $('.TitleAni2');
+            if (textWrapper.length > 0) {
+                textWrapper.html(textWrapper.text().replace(/\S/g, "<span class='TitleAnimation2'>$&</span>"));
+
+                anime.timeline({loop: true})
+                    .add({
+                        targets: '.TitleAnimation2',
+                        scale: [0.3, 1],
+                        opacity: [0, 1],
+                        translateZ: 0,
+                        easing: "easeOutExpo",
                         duration: 500,
                         delay: (el, i) => 70 * (i + 1)
                     }).add({
-                        targets: '.TitleAnimation',
+                        targets: '.TitleAnimation2',
                         opacity: 0,
                         duration: 800,
                         easing: "easeOutExpo",
                         delay: 800
                     });
             } else {
-                console.error("Element with class 'TitleAni' not found.");
+                console.error("Element with class 'TitleAni2' not found.");
             }
-        }
-    });
-});
+})
