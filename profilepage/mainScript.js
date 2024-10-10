ea039db2b434252ccb003c7579a0c76d17de48e4
eg_kim <kimsagwa00@gmail.com>
2024-10-10 17:06:03 +0900

    secondPage Title Animation

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                         opacity: 0,
                         duration: 900,
                         easing: "easeOutExpo",
-                        delay: 1100
+                        delay: 1500
                     });
             } else {
                 console.error("Element with class 'TitleAni' not found.");
@@ -33,16 +33,38 @@ $(document).ready(function(){
                         opacity: [0, 1],
                         translateZ: 0,
                         easing: "easeOutExpo",
-                        duration: 500,
+                        duration: 900,
                         delay: (el, i) => 70 * (i + 1)
                     }).add({
                         targets: '.TitleAnimation2',
                         opacity: 0,
-                        duration: 800,
+                        duration: 900,
                         easing: "easeOutExpo",
-                        delay: 800
+                        delay: 500
                     });
             } else {
                 console.error("Element with class 'TitleAni2' not found.");
-            }
+            };
+
+            const $counters = $('.scroll_on');
+            const exposurePercentage = 100;
+            const loop = true;
+            
+            $(window).on('scroll', function() {
+                $counters.each(function() {
+                    const $el = $(this);
+                    const rect = $el[0].getBoundingClientRect();
+                    const winHeight = window.innerHeight;
+                    const contentHeight = rect.bottom - rect.top;
+            
+                    // 요소가 화면에 노출되었을 때
+                    if (rect.top <= winHeight - (contentHeight * exposurePercentage / 100)) {
+                        $el.addClass('active'); // active 클래스 추가
+                    } 
+                    // loop가 true이고 요소가 화면에서 벗어났을 때
+                    if (loop && (rect.bottom <= 0 || rect.top >= window.innerHeight)) {
+                        $el.removeClass('active'); // active 클래스 제거
+                    }
+                });
+            });
 })
