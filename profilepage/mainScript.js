f5329709c8c253a2e3b8bf4f9cbcb11c5c91dba7
eg_kim <kimsagwa00@gmail.com>
2024-10-15 16:07:09 +0900

    third page script

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -68,5 +68,36 @@ $(document).ready(function(){
                 });
             }).scroll();
             
+            // var popupData = [
+            //     {id: 'popUp1', content: "hello"},
+            //     {id: 'popUp2', content: "hello"},
+            //     {id: 'popUp3', content: "hello"},
+            // ]
            
+            // popupData.forEach(
+            //     function(){
+            //         $('')
+            //     }
+            // )    
+
+            var thirdCont = [
+                {id: "thirdBox1", title:"Team Project", image:"./profilepage/images/mail.png", alt:"매일유업", boxtitle:"매일유업 RENEWAL", className:"thirdFirst", colorName:"gray", colorName2:"colorBlue", firstH3:"Team" },
+                {id: "thirdBox2", title:"Team project", image:"./profilepage/images/logo-light.png", alt:"세탁왕", boxtitle:"세탁왕 RENEWAL", className:"thirdSecond", colorName:"colorBlack", firstH3:"Team"},
+                {id: "thirdBox3", title:"Team project", image:"./profilepage/images/heroesshoptlogo.png", alt:"히어로즈샵", boxtitle:"Heroes Shop", className:"thirdThird", colorName:"colorBlue", firstH3:"Personer"},
+            ]
+            thirdCont.forEach(function(data){
+                $('#thirdList').append(`
+                    <li id="${data.id}">
+                        <div class="thirdBox ${data.className}">
+                            <h3><span class="${data.colorName}">${data.firstH3}</span> <span class="${data.colorName2}">Project</span>
+                            <div class="fontNeo textWrap">
+                                <img src="${data.image}" alt="${data.alt}" />
+                                <p>${data.boxtitle}</p>
+                                <a href="#none">project page <span class="material-symbols-outlined">attach_file</span></a>
+                                <a href="#none">ppt <span class="material-symbols-outlined">attach_file</span></a>
+                            </div>
+                        </div>
+                    </li>`);
+       
+            })
 });
