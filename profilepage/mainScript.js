ef39ab38ff7201e54505a158880c4bc69d9886ca
eg_kim <kimsagwa00@gmail.com>
2024-10-15 17:54:37 +0900

    popupCont not complete

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -89,4 +89,11 @@ $(document).ready(function(){
                     </li>`);
        
             })
+
+            var popupCont = [
+                {id: "popup1", popH3:"매일유업 리뉴얼 프로젝트", popImage:"./profilepage/images/mail.png", popAlt:"매일유업", teamSpan:"About Our Project", popDate:"2024. 06.12 Wed - 2024. 07. 05 Fri", Tools:"Html5, Css3, JavaScript, JQuery", layoutType:"fixed", con1:"1. 메인 페이지 이미지 및 레이아웃 디자인", con2:"2. 메인 페이지 5-6섹션 및 footer 제작", con3:"3. 기업소개, 브랜드, 로그인 페이지 디자인 및 제작", con4:"4. 프로젝트 진행 총괄", pjLink:"#none", ppt:"#none" },
+                {id: "popup2", popH3:"세탁왕 리뉴얼 프로젝트", popImage:"./profilepage/images/logo-light.png", popAlt:"세탁왕", teamSpan:"About Our Project", popDate:"2024. 07.31 Wed - 2024. 08. 14 Wed", Tools:"Vue.js, Vuetify, Firebase", layoutType:"Responsive", con1:"1. 요금표, 공지사항, 자주묻는질문, 사이드바 디자인", con2:"2. 이용내역, 요금표, 공지사항, 자주묻는질문 제작", con3:"3. 프로젝트 진행 총괄", pjLink:"#none", ppt:"#none" },
+
+                {id: "popup3", }
+            ]
 });
