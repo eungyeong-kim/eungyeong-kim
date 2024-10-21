6a8d50c79489cabda346296c98bb1d9fb74789aa
eg_kim <kimsagwa00@gmail.com>
2024-10-21 10:20:58 +0900

    modal code upgrade

--- profilepage/mainScript.js
+++ profilepage/mainScript.js
@@ -93,11 +93,55 @@ $(document).ready(function(){
 
             //popup
             var popupCont = [
-                {id: "popup1", popH3:"매일유업 리뉴얼 프로젝트", popImage:"./profilepage/images/mail.png", popAlt:"매일유업", projectTypeTitle:"About Our Project", popDate:"2024. 06.12 Wed - 2024. 07. 05 Fri", Tools:"Html5, Css3, JavaScript, JQuery", layoutType:"fixed", con1:"1. 메인 페이지 이미지 및 레이아웃 디자인", con2:"2. 메인 페이지 5-6섹션 및 footer 제작", con3:"3. 기업소개, 브랜드, 로그인 페이지 디자인 및 제작", con4:"4. 프로젝트 진행 총괄", pjLink:"#none", ppt:"#none", projectType:"team" },
-                {id: "popup2", popH3:"세탁왕 리뉴얼 프로젝트", popImage:"./profilepage/images/logoDark.png", popAlt:"세탁왕", projectTypeTitle:"About Our Project", popDate:"2024. 07.31 Wed - 2024. 08. 14 Wed", Tools:"Vue.js, Vuetify, Firebase", layoutType:"Responsive", con1:"1. 요금표, 공지사항, 자주묻는질문, 사이드바 디자인", con2:"2. 이용내역, 요금표, 공지사항, 자주묻는질문 제작", con3:"3. 프로젝트 진행 총괄", pjLink:"#none", ppt:"#none", projectType:"team" },
-                {id: "popup3", popH3:"히어로즈샵 프로젝트", popImage:"./profilepage/images/heroesShopGray.png", popAlt:"히어로즈샵", projectTypeTitle:"About My Project", popDate:"2024. 09.04 Wed - 2024. 09. 24 Tue", Tools:"React, React BootStrap, FireBase", layoutType:"Responsive", pjLink:"#none", ppt:"#none", projectType:"personal"}
+                {id: "popup1",
+                popH3:"매일유업 리뉴얼 프로젝트",
+                popImage:"./profilepage/images/mail.png",
+                popAlt:"매일유업",
+                projectTypeTitle:"About Our Project",
+                popDate:"2024. 06.12 Wed - 2024. 07. 05 Fri",
+                Tools:"Html5, Css3, JavaScript, JQuery",
+                layoutType:"fixed",
+                contribution:["1. 메인 페이지 이미지 및 레이아웃 디자인", "2. 메인 페이지 5-6섹션 및 footer 제작", "3. 기업소개, 브랜드, 로그인 페이지 디자인 및 제작", "4. 프로젝트 진행 총괄"],
+                pjLink:"#none", 
+                ppt:"#none", 
+                projectType:"team" },
+                
+                {id: "popup2", 
+                popH3:"세탁왕 리뉴얼 프로젝트", 
+                popImage:"./profilepage/images/logoDark.png", 
+                popAlt:"세탁왕", 
+                projectTypeTitle:"About Our Project", 
+                popDate:"2024. 07.31 Wed - 2024. 08. 14 Wed", 
+                Tools:"Vue.js, Vuetify, Firebase", 
+                layoutType:"Responsive",
+                contribution:["1. 요금표, 공지사항, 자주묻는질문, 사이드바 디자인", "2. 이용내역, 요금표, 공지사항, 자주묻는질문 제작", "3. 프로젝트 진행 총괄"],
+                pjLink:"#none", 
+                ppt:"#none", 
+                projectType:"team" },
+                
+                {id: "popup3", 
+                popH3:"히어로즈샵 프로젝트", 
+                popImage:"./profilepage/images/heroesShopGray.png", 
+                popAlt:"히어로즈샵", 
+                projectTypeTitle:"About My Project", 
+                popDate:"2024. 09.04 Wed - 2024. 09. 24 Tue", 
+                Tools:"React, React BootStrap, FireBase", 
+                layoutType:"Responsive", 
+                pjLink:"#none", 
+                ppt:"#none", 
+                projectType:"personal"}
             ]
+
             popupCont.forEach(function(popupContData, index){
+                
+                var contData = '';
+                if(Array.isArray(popupContData.contribution)){
+                popupContData.contribution.forEach(function(item){
+                    contData += `<li>${item}</li>`;
+                });}else{
+                    '';
+                }
+
                 if(popupContData.projectType==='team'){
                 $('body').append(`
                     <div class="popupWidth" id="${popupContData.id}" data-index="${index}" style="display:none">
@@ -116,10 +160,7 @@ $(document).ready(function(){
                                         <p class="popupLine" aria-hidden="true">****************************************</p>
                                         <p>CONTRIBUTION</p>
                                         <ul>
-                                            <li>${popupContData.con1}</li>
-                                            <li>${popupContData.con2}</li>
-                                            <li>${popupContData.con3}</li>
-                                            <li>${popupContData.con4}</li>
+                                            ${contData}
                                         </ul>
                                         <p class="popupLine" aria-hidden="true">****************************************</p>
                                         <p>Link To</p>
